6ba0365e7b4f2aa20e8d615a281c8be05cd724ab
pinn <supapon.sp@mail.kmutt.ac.th>
2021-03-28 10:23:34 +0700

    ข้อสอบ2

---
+++ ข้อ2..cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ข้อ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+            while (x > y || x < y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+
+            }
+            if (x == y) { Console.WriteLine(x); }
+            else { Console.WriteLine(""); }
+
+
+        }
+    }
+}
